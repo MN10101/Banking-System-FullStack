392da7a7d83cf28f514b59e86684262d486fbee4
Mahmoud Najmeh <mamocool3@gmail.com>
2026-02-24 11:06:32 +0100

    Delete src directory

--- src/main/java/com/nexgen/bankingsystem/service/ShoppingService.java
+++
@@ -1,6 +0,0 @@
-package com.nexgen.bankingsystem.service;
-
-public interface ShoppingService {
-    boolean processPurchase(String accountNumber, double amount, String currency, String cardNumber, String expMonth, String expYear, String cvc);
-}
-
